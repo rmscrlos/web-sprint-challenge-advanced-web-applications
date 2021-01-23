101694753dcfcaba39f412511660a90642425ef0
Jean Carlos  Ramos <rmscrlos@gmail.com>
2021-01-23 13:40:10 -0500

    completed mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './components/PrivateRoute';
+import './styles.scss';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/" component={Login} />
+				{/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-      </div>
-    </Router>
-  );
+				<PrivateRoute exact path="/bubblepage">
+					<BubblePage />
+				</PrivateRoute>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
